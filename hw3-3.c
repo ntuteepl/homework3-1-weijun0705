fd91b7efc209dee19c58d65974f7adb62e4060a8
weijun0705 <146065151+weijun0705@users.noreply.github.com>
2023-10-23 21:40:29 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int digitSum(int n)
+{
+    int k=0;
+    while (n>0)
+{
+    k+=n%10;
+    n/=10;
+}
+    return k;
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int kA=digitSum(a);
+    int kB=digitSum(b);
+
+    if (kA<kB||(kA==kB &&a<b))
+{
+        printf("%d", a);
+}
+    else
+{
+        printf("%d", b);
+}
+}
